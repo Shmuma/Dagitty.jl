a949f3ba456f3959cf4f617fb48655414b68e41f
Max Lapan <max.lapan@gmail.com>
2021-08-26 15:00:22 +0200

    Take empty conditional set into account

--- src/implied_conditional_independencies.jl
+++ src/implied_conditional_independencies.jl
@@ -36,13 +36,9 @@ function implied_conditional_independencies(dag::DAG)::Vector{ConditionalIndepen
     max_set = length(dag.labels) - 2
     result = Vector{ConditionalIndependence}()
     nodes = Base.OneTo(length(dag.labels))
-    # TODO: check that combinations is enough for check. I have a feeling we have to check all unordered pairs of nodes,
-    # but combinations returns only ordered ones
     for (n1, n2) ∈ combinations(nodes, 2)
-    # for n1 ∈ nodes, n2 ∈ nodes
-    #     n1 == n2 && continue
         rest = [n for n ∈ nodes if n != n1 && n != n2]
-        for cond ∈ powerset(rest, 1, max_set)
+        for cond ∈ powerset(rest)
             if is_d_separated(dag, [n1], [n2], cond)
                 ci = ConditionalIndependence(dag.labels[n1], dag.labels[n2], dag.labels[cond])
                 push!(result, ci)
