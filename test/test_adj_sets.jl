1c7353e2e5d3ce7baff90a3cd2758e1ca9072462
Max Lapan <max.lapan@gmail.com>
2021-08-31 19:07:28 +0200

    Extra tests

--- test/test_adj_sets.jl
+++ test/test_adj_sets.jl
@@ -5,3 +5,11 @@ g = DAG(:X => :Y, :U => :X, :A => :U, :A => :C, :C => :Y, :U => :B, :C => :B)
 @test is_valid_backdoor_adjustment_set(g, node(g, :X), node(g, :Y), nodes_indices(g, [:C]))
 @test is_valid_backdoor_adjustment_set(g, node(g, :X), node(g, :Y), nodes_indices(g, [:A]))
 @test is_valid_backdoor_adjustment_set(g, node(g, :X), node(g, :Y), nodes_indices(g, [:U]))
+
+# Waffles example from Rethinking book
+g = DAG(:S => :W, :S => :A, :S => :M, :A => :M, :M => :D, :A => :D, :W => :D)
+@test all_backdoor_adjustment_sets(g, :W, :D) == [[:S], [:A, :M]]
+
+# Another example
+g = DAG(:E => :W, :U => :W)
+@test all_backdoor_adjustment_sets(g, :E, :W) == [Symbol[]]
